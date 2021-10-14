ad450a65057d19e42ef12a8c4ae864a45afb522f
Jorge Arguelles <digitalandmedialabs@gmail.com>
2021-10-14 13:54:38 -0500

    Ajustes en el menu

--- js/inversionistas.js
+++ js/inversionistas.js
@@ -1,6 +1,14 @@
 console.log("inv:rdy");
-
-jQuery('#inicio-slider').slick({
-	arrows: false,
-	dots: true
+jQuery(document).ready(function($){
+	// Cambio de color del nav con el scroll
+	$(document).scroll(function () {
+		console.log('scolling');
+		var $nav = $("#inversionistas-navegacion");
+		$nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+	});
+	// Configuracion slider inicio
+	$('#inicio-slider').slick({
+		arrows: false,
+		dots: true
+	});
 });
